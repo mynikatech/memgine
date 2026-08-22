17e092c3c084f96c2841cfa301634fd37e15ee9c
sunil <apnafundaws@gmail.com>
2026-08-23 00:19:32 +0530

    index change to remove mock services and the data model file

--- frontend/src/core/index.ts
+++ frontend/src/core/index.ts
@@ -25,8 +25,6 @@ export * from "./defaults/glow-studio";
 export * from "./defaults/glow-studio-content";
 export * from "./defaults/business-registry";
 export * from "./defaults/business-content";
-export * from "./mocks/mock-services";
-export * from "./mocks/mock-reference-data";
 export * from "./customer/customer-registration";
 export * from "./defaults/default-business-template";
 export * from "./organization/organization-onboarding";
